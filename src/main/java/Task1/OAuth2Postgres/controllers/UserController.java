d451791c318b233b6a1a522db0729596a66fa1b9
NickKim1225 <nicholas.kim@jarwarellc.com>
2019-10-07 13:42:27 -0400

    with post to dig sig MS

--- src/main/java/Task1/OAuth2Postgres/controllers/UserController.java
+++ src/main/java/Task1/OAuth2Postgres/controllers/UserController.java
@@ -1,9 +1,14 @@
 package Task1.OAuth2Postgres.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import Task1.OAuth2Postgres.repositories.UsersRepository;
 
@@ -25,7 +30,12 @@ public class UserController {
 	}
 	
 	@GetMapping("/check")
-	public String privateArea() {
-		return "PRIVATE AREA";
+	public String privateArea(@RequestBody String msg) throws Exception{
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Content-Type", "application/json");
+		HttpEntity<String> entity = new HttpEntity<String>(msg, headers); 
+		String response = new RestTemplate().postForObject("http://localhost:5000/sign", entity, String.class);
+		
+		return response;
 	}
 }
